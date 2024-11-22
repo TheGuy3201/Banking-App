4d07b6c801e5ffe4f4fc2828095ff0af73145040
joshd <joshd@JOSHUAS_NEW_PC>
2024-11-22 17:20:37 -0500

    Add project files.

---
+++ Banking Application/ITransaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Application
+{
+    internal interface ITransaction
+    {
+        void Withdraw(double amount)
+        {
+
+        }
+
+        void Deposit(double amount)
+        {
+
+        }
+    }
+}
