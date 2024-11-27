0dd65607a1bd21f2288cfd8c4a33baa480aed3ee
TheGuy3025 <148494353+TheGuy3201@users.noreply.github.com>
2024-11-27 17:31:30 -0500

    JoshStuff

---
+++ Banking Application/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Application
+{
+    class Person
+    {
+        private string password;
+        public event EventHandler OnLogin;
+
+        public string Sin { get; }
+        public string Name { get; }
+        public bool IsAuthenticated { get; private set; }
+
+        public Person(string name, string sin)
+        {
+            Name = name;
+            Sin = sin;
+        }
+    }
+}
