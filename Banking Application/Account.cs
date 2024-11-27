243bb724272dba6e62bae673d34b7ef4299c49e4
eyalbalilty <eyalbalilty@gmail.com>
2024-11-27 16:39:56 -0500

    Created accout class

--- Banking Application/Account.cs
+++ Banking Application/Account.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Banking_Application
 {
-    internal class Account
+    internal abstract class Account
     {
+        private static int LAST_NUMBER = 100_000;
+        protected readonly List<Person> users = new List<Person>();
+        public readonly List<Transaction> transactions = new List<Transaction>();
+        public event EventHandler OnTransaction;
+
+
     }
 }
