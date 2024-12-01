5d8aeb5e7a0e66f8a01f5fe37636ab243d02df62
EvolvedCastle <ragulanchandrasegaran@gmail.com>
2024-11-30 21:04:44 -0500

    Fixed Itransaction
    Note to self:
    If its a interface, you only just need to declare it, no need for extra stuff.

--- Banking Application/ITransaction.cs
+++ Banking Application/ITransaction.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 namespace Banking_Application
 {
     
-    internal interface ITransaction //if I make a class its different
+    internal interface ITransaction //if I make a class its different (replace interface with class :moyai:
     {
-        void Withdraw(decimal amount, Person person)
-        {
-            
-        }
-
-        void Deposit(decimal amount, Person person)
-        {
-
-        }
+        void Withdraw(decimal amount, Person person);
+        void Deposit(decimal amount, Person person);
+        
+        /*All I had to do is simply remove the brackets. Why?:
+         *Since we're working with interfaces, we only just need to declare methods
+         *The reason why is because the Account Classes look back on this interface to use
+         *I'll be proved wrong later when it matters, but think of it as a constant, but your applying it to a whole class- 
+         *so henceforth, its a interface.
+         */
 
     }
 }
